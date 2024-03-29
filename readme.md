de7ee673852b8f00e0d4c738bc48dc00c7103ee3
Praful Gulani <prafulgulani555@gmail.com>
2024-03-29 15:16:42 +0530

    version 2

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 ## Version 1
 - Used basic things like if-else to decide the winner, sys to exit the program, random for computer choice, enum for number and rps relation. 
+
+## Version 2
+- Included a while loop so that a user can play as long as he wishes to continue.
+- The game continues if user types Y for yes and ends if user types Q for quit 
--- rps.py
+++ rps.py
@@ -7,30 +7,42 @@ class RPS(Enum):
     PAPER = 2
     SCISSORS = 3
 
-print("")
-player_choice = input("Enter ... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n")
+play_again = True
 
-player = int(player_choice)
+while play_again:
 
-if player < 1 or player > 3:
-    sys.exit("You must enter 1,2, or 3.")
+    player_choice = input("\nEnter ... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n")
 
-computer_choice = random.choice("123")
+    player = int(player_choice)
 
-computer = int(computer_choice)
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1,2, or 3.")
 
-print("")
-print("You chose " + str(RPS(player)).replace('RPS.', "") + ".")
-print("Computer chose " + str(RPS(computer)).replace('RPS.', "") + ".")
-print("")
+    computer_choice = random.choice("123")
 
-if player == 1 and computer == 3:
-    print("You Win 🥳")
-elif player == 2 and computer == 1:
-    print("You Win 🥳")
-elif player == 3 and computer == 2:
-    print("You Win 🥳")
-elif player == computer:
-    print("Tie Game 🙌")
-else: 
-    print("Python Wins 🐍")
+    computer = int(computer_choice)
+
+    print("\nYou chose " + str(RPS(player)).replace('RPS.', "") + ".")
+    print("Computer chose " + str(RPS(computer)).replace('RPS.', "") + ".\n")
+
+    if player == 1 and computer == 3:
+        print("You Win 🥳")
+    elif player == 2 and computer == 1:
+        print("You Win 🥳")
+    elif player == 3 and computer == 2:
+        print("You Win 🥳")
+    elif player == computer:
+        print("Tie Game 🙌")
+    else: 
+        print("Python Wins 🐍")
+
+        play_again = input("\nPlay Again? \nY for Yes or \n Q to Quit \n\n")
+
+        if play_again.lower() == "y":
+            continue
+        else:
+            print("\n🎉🎉🎉")
+            print("Thank you for Playing\n")
+            play_again = False
+
+sys.exit("Bye 👋")
